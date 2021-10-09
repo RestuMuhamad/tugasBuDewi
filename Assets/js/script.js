338c3fa4f212b06bda3f8d03524d741d47c388f1
Restu muhamad <restumuhamad25@gmail.com>
2021-10-09 20:20:48 +0700

    Update script.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -57,7 +57,7 @@ window.addEventListener("load", () => {
 		const cardBuah = el;
 		cardBuah.classList.toggle("active");
 		setTimeout(() => {
-			cardBuah.style.background = "url('Assets/jadi/" + angkaEl + "-1.png')";
+			cardBuah.style.background = "url('tugasBuDewi/Assets/jadi/" + angkaEl + "-1.png')";
 			cardBuah.style.backgroundSize = "contain";
 			cardBuah.style.backgroundRepeat = "no-repeat";
 			cardBuah.classList.toggle("active");
